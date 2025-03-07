aaa0e07c9d5ef42d357b62b0306f3d76684bc7ac
mejomba <moojaminzadeh@gmail.com>
2025-03-07 16:31:06 +0330

    add category model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from aaa.models import CustomUser
+
+
+class AbstractBaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    delete_date = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_special = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def jcreated_date(self):
+        return 'jcreated_date'
+
+    def jupdate_date(self):
+        return 'jupdate_date'
+
+
+class AbstractBaseWithUserModel(AbstractBaseModel):
+    creator_user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, related_name='creators')
+    editor_user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, related_name='editors')
+
+    class Meta:
+        abstract = True
+
+
+class Category(AbstractBaseWithUserModel):
+    title = models.CharField(max_length=128)
+    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, related_name='cats')
+    is_menu = models.BooleanField(default=False)
+
