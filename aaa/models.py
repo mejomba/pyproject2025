b5e6eef09e7efeaef83482acedf0da388a2434e1
mejomba <moojaminzadeh@gmail.com>
2025-03-07 15:37:12 +0330

    customize user model for username_field

--- aaa/models.py
+++ aaa/models.py
@@ -1,6 +1,19 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    username = None
+    phone = models.CharField(verbose_name='تلفن همراه', max_length=11, unique=True, null=True, blank=True,
+                             validators=[RegexValidator(r'^09\d{9}',
+                                                        message='تلفن نا معتبر',
+                                                        code='invalid_phone')])
+    update_date = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    delete_date = models.DateTimeField(null=True, blank=True)
+    is_special = models.BooleanField(default=False)
+    creator_user = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True, related_name='creators')
+    editor_user = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True, related_name='editors')
+
+    USERNAME_FIELD = 'phone'
