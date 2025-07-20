62573aa120d29f53bb7e94869c1bbf5d7a26178f
mejomba <moojaminzadeh@gmail.com>
2025-07-20 17:11:31 +0330

    change: add instructor to course serializer

--- courses/serializers/course.py
+++ courses/serializers/course.py
@@ -16,7 +16,7 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'title', 'slug', 'description',
             'price', 'image', 'category', 'status',
-            'created_at', 'updated_at',
+            'created_at', 'updated_at', 'instructor',
             'creator_user', 'editor_user'
         ]
         read_only_fields = ['id', 'slug', 'created_at', 'updated_at', 'creator_user', 'editor_user']
