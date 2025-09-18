e4a2ecdc2e281fadf2b6c109a0ea72da857f2640
mejomba <moojaminzadeh@gmail.com>
2025-09-18 12:18:17 +0330

    use model admin for blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from blog.models import post
 
 
-admin.site.register(post.Post)
+@admin.register(post.Post)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "created_at", "publish_at")
+
+    def get_queryset(self, request):
+        return post.Post.objects.all()
+
